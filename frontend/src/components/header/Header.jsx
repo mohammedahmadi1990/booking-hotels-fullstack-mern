5c87217cedbf9d32dc7646069e9a36d7d162c45a
Med <mohammed.ahmadi1990@gmail.com>
2022-08-16 10:11:58 +0300

    routes created

---
+++ frontend/src/components/header/Header.jsx
@@ -0,0 +1,18 @@
+// import { faBed } from '@fortawesome/free-regular-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import React from 'react'
+import "./header.css"
+
+function Header() {
+  return (
+    <div className='header'>
+      <div className="headerList">
+        <div className="headerListItem">
+        {/* <FontAwesomeIcon icon={faBed} /> */}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Header
