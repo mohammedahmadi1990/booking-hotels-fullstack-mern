941d966aed940f62783b26af0c28a2fe6bb9729b
Med <mohammed.ahmadi1990@gmail.com>
2022-08-31 09:25:11 +0300

    Frontend done!

--- frontend/src/pages/hotel/Hotel.jsx
+++ frontend/src/pages/hotel/Hotel.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import "./hotel.css";
 import Navbar from "../../components/navbar/Navbar";
 import Header from "../../components/header/Header";
@@ -6,34 +5,35 @@ import MailList from "../../components/mailList/MailList";
 import Footer from "../../components/footer/Footer";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
-  faLocationDot,
-  faCircleXmark,
   faCircleArrowLeft,
   faCircleArrowRight,
+  faCircleXmark,
+  faLocationDot,
 } from "@fortawesome/free-solid-svg-icons";
 import { useState } from "react";
 
 const Hotel = () => {
   const [slideNumber, setSlideNumber] = useState(0);
   const [open, setOpen] = useState(false);
+
   const photos = [
     {
-      src: "https://cf.bstatic.com/xdata/images/hotel/max1280x900/346521349.jpg?k=1dee54f2b60b78d84c0ea9bb44c3e547c4437c3525fa6b18391ea75f6cef4e56&o=&hp=1",
+      src: "https://cf.bstatic.com/xdata/images/hotel/max1280x900/261707778.jpg?k=56ba0babbcbbfeb3d3e911728831dcbc390ed2cb16c51d88159f82bf751d04c6&o=&hp=1",
     },
     {
-      src: "https://cf.bstatic.com/xdata/images/hotel/max1280x900/346521333.jpg?k=354c2e47a62c3a1e7eb1f7e377839cd31d0e02023c3f09579129b6fc7abba199&o=&hp=1",
+      src: "https://cf.bstatic.com/xdata/images/hotel/max1280x900/261707367.jpg?k=cbacfdeb8404af56a1a94812575d96f6b80f6740fd491d02c6fc3912a16d8757&o=&hp=1",
     },
     {
-      src: "https://cf.bstatic.com/xdata/images/hotel/max1280x900/346521349.jpg?k=1dee54f2b60b78d84c0ea9bb44c3e547c4437c3525fa6b18391ea75f6cef4e56&o=&hp=1",
+      src: "https://cf.bstatic.com/xdata/images/hotel/max1280x900/261708745.jpg?k=1aae4678d645c63e0d90cdae8127b15f1e3232d4739bdf387a6578dc3b14bdfd&o=&hp=1",
     },
     {
-      src: "https://cf.bstatic.com/xdata/images/hotel/max1280x900/346520925.jpg?k=98a39040d91d21a3f8d513c5c0ce0958c8bc4df5cb5897b0b8521c07835d8e61&o=&hp=1",
+      src: "https://cf.bstatic.com/xdata/images/hotel/max1280x900/261707776.jpg?k=054bb3e27c9e58d3bb1110349eb5e6e24dacd53fbb0316b9e2519b2bf3c520ae&o=&hp=1",
     },
     {
-      src: "https://cf.bstatic.com/xdata/images/hotel/max1280x900/349227264.jpg?k=0965c8474c305ccad5895a079e0da4d1247032d1419319f62dd0ff8fe1e9b2de&o=&hp=1",
+      src: "https://cf.bstatic.com/xdata/images/hotel/max1280x900/261708693.jpg?k=ea210b4fa329fe302eab55dd9818c0571afba2abd2225ca3a36457f9afa74e94&o=&hp=1",
     },
     {
-      src: "https://cf.bstatic.com/xdata/images/hotel/max1280x900/349227292.jpg?k=0bec2ae992aed8d33c60c1baf607611d705dad5156857eb3a8d34cb003ed6795&o=&hp=1",
+      src: "https://cf.bstatic.com/xdata/images/hotel/max1280x900/261707389.jpg?k=52156673f9eb6d5d99d3eed9386491a0465ce6f3b995f005ac71abc192dd5827&o=&hp=1",
     },
   ];
 
@@ -41,6 +41,19 @@ const Hotel = () => {
     setSlideNumber(i);
     setOpen(true);
   };
+
+  const handleMove = (direction) => {
+    let newSlideNumber;
+
+    if (direction === "l") {
+      newSlideNumber = slideNumber === 0 ? 5 : slideNumber - 1;
+    } else {
+      newSlideNumber = slideNumber === 5 ? 0 : slideNumber + 1;
+    }
+
+    setSlideNumber(newSlideNumber);
+  };
+
   return (
     <div>
       <Navbar />
@@ -48,30 +61,42 @@ const Hotel = () => {
       <div className="hotelContainer">
         {open && (
           <div className="slider">
-            <FontAwesomeIcon icon={faCircleXmark} />
-            <FontAwesomeIcon icon={faCircleArrowLeft} />
+            <FontAwesomeIcon
+              icon={faCircleXmark}
+              className="close"
+              onClick={() => setOpen(false)}
+            />
+            <FontAwesomeIcon
+              icon={faCircleArrowLeft}
+              className="arrow"
+              onClick={() => handleMove("l")}
+            />
             <div className="sliderWrapper">
               <img src={photos[slideNumber].src} alt="" className="sliderImg" />
             </div>
-            <FontAwesomeIcon icon={faCircleArrowRight} />
+            <FontAwesomeIcon
+              icon={faCircleArrowRight}
+              className="arrow"
+              onClick={() => handleMove("r")}
+            />
           </div>
         )}
         <div className="hotelWrapper">
           <button className="bookNow">Reserve or Book Now!</button>
-          <h1 className="hotelTitle">Grand Hotel</h1>
+          <h1 className="hotelTitle">Tower Street Apartments</h1>
           <div className="hotelAddress">
             <FontAwesomeIcon icon={faLocationDot} />
-            <span>Elton st 125 New York</span>
+            <span>Elton St 125 New york</span>
           </div>
           <span className="hotelDistance">
-            Excellent location - 500m from center
+            Excellent location – 500m from center
           </span>
-          <span className="hotlePriceHighlight">
+          <span className="hotelPriceHighlight">
             Book a stay over $114 at this property and get a free airport taxi
           </span>
           <div className="hotelImages">
             {photos.map((photo, i) => (
-              <div className="hotelImgWrapper">
+              <div className="hotelImgWrapper" key={i}>
                 <img
                   onClick={() => handleOpen(i)}
                   src={photo.src}
@@ -82,8 +107,8 @@ const Hotel = () => {
             ))}
           </div>
           <div className="hotelDetails">
-            <div className="hotelDetailsText">
-              <h1 className="hotelTitle">Stay in the heart of Krakow</h1>
+            <div className="hotelDetailsTexts">
+              <h1 className="hotelTitle">Stay in the heart of City</h1>
               <p className="hotelDesc">
                 Located a 5-minute walk from St. Florian's Gate in Krakow, Tower
                 Street Apartments has accommodations with air conditioning and
@@ -93,19 +118,19 @@ const Hotel = () => {
                 also offered, as well as an electric tea pot and a coffee
                 machine. Popular points of interest near the apartment include
                 Cloth Hall, Main Market Square and Town Hall Tower. The nearest
-                airport isJohn Paul II International Krakéw—Balice, 16.1 km from
-                Tower Street Apartments, and the property offers a paid airport
-                shuttle service.
+                airport is John Paul II International Kraków–Balice, 16.1 km
+                from Tower Street Apartments, and the property offers a paid
+                airport shuttle service.
               </p>
             </div>
             <div className="hotelDetailsPrice">
               <h1>Perfect for a 9-night stay!</h1>
               <span>
-                Located in the real heart of Karkow, this property has an
-                excellent location score of 8.9!
+                Located in the real heart of Krakow, this property has an
+                excellent location score of 9.8!
               </span>
               <h2>
-                <b>$947</b> (9 nights)
+                <b>$945</b> (9 nights)
               </h2>
               <button>Reserve or Book Now!</button>
             </div>
