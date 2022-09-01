41a9e7b6d353da3e5404f22a66f1722e12e21999
Med <mohammed.ahmadi1990@gmail.com>
2022-09-01 14:25:32 +0300

    mongoDb added + first request

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,32 @@
 import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
 
 const app = express();
+dotenv.config();
+
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO);
+    console.log("conncted to MongoDB!");
+  } catch (error) {
+    throw error;
+  }
+};
+
+mongoose.connection.on("disconnected", () => {
+  console.log("MongoDB disconncted!");
+});
+
+mongoose.connection.on("connected", () => {
+  console.log("MongoDB disconncted!");
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello from ExpressJS");
+});
 
 app.listen("8800", () => {
+  connect();
   console.log("connected to backend!");
 });
